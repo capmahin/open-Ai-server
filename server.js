cf4b54b61c53df8e057320fa827125ad2027fb3d
capmahin <mahinulhuque1996@gmail.com>
2024-09-21 22:54:59 +0600

    configuration

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ import cors from 'cors';
 import {Configuration, OpenAIApi} from 'openai';
 
 dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+
+});
+
+const openai = new OpenAIApi(configuration);
